ec598c3112511d1e7c07e70e2dd045f87a21b488
Taras Yurchyshyn <yts.prof@gmail.com>
2024-02-18 12:47:27 +0200

    Quick start launched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+
+const Header = () => {
+  return (
+    <header className='App'>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return (
+//     <input type="text" 
+//     placeholder={holder} 
+//     style={styledField}/>
+//   )
+// }
+
+class Field extends Component {
+  render() {
+      const holder = 'Enter here';
+      const styledField = {
+        width: '300px'
+      };
+    return <input type="text" 
+          placeholder={holder} 
+          style={styledField}/>
+  }
+}
+
+function Btn () {
+  const text = 'Log in';
+  const logged = false;
+  // const res = () =>{
+  //   return 'Add player';
+  // }
+  //const p = <p>Log in</p>;
+  return (
+    <button>{logged ? 'Enter' : text}</button>
+  )
+}
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
+
     </div>
   );
 }
 
+export {Header};
 export default App;
